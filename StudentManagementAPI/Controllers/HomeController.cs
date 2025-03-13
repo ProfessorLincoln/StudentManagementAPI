60cd726f03bdfe3171f15695e254213f93915803
m9dnor <danial191003@gmail.com>
2025-03-14 00:09:51 +0800

    aasd

--- StudentManagementAPI/Controllers/HomeController.cs
+++ StudentManagementAPI/Controllers/HomeController.cs
@@ -29,4 +29,4 @@ namespace StudentManagementAPI.Controllers
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
-}sfzsdc
+}
